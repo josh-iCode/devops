b515f20746c163feb1e8bef2ebe63f0224c2a623
EC2 Default User <ec2-user@ip-172-31-28-28.us-west-2.compute.internal>
2020-06-05 17:33:01 +0000

    Added CircleCI

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=local_docker . 
+docker build --tag=mydockerproj . 
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 local_docker
+docker run -p 8000:80 mydockerprojdocker
